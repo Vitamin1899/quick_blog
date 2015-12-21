b517e85cc331ce24cce6198aac8a5977a9133761
Vitamin1899 <vitalimarinich@gmail.com>
2015-12-21 11:43:26 +0300

    added comments for post in views; istalled gem pry

--- Gemfile
+++ Gemfile
@@ -6,6 +6,10 @@ gem 'rails', '4.0.1'
 # Use postgresql as the database for Active Record
 gem 'pg'
 
+group :test, :development do
+  gem 'pry'
+end
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,14 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create!(params[:comment])
+    #binding.pry
+    @comment = @post.comments.create!(comment_params)
     redirect_to @post
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 
 end
